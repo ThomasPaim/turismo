146bb3b13124ec42ad366cc55227128ac0e55fc3
Thomas Paim <107310282+ThomasPaim@users.noreply.github.com>
2024-07-24 15:25:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('increase-font').addEventListener('click', function() {
+    document.body.style.fontSize = 'larger';
+});
+
+document.getElementById('decrease-font').addEventListener('click', function() {
+    document.body.style.fontSize = 'smaller';
+});
